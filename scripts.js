55ebbde7252c1e93f63d71d4249021b26eddeab9
Lakhan-03 <87630215+Lakhan-03@users.noreply.github.com>
2021-12-14 17:51:20 +0530

    testing changes

--- scripts.js
+++ scripts.js
@@ -5,5 +5,5 @@ setInterval(() => {
     a = new Date();
     time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
     date = a.toDateString();
-    document.getElementById('time').innerHTML = time + " :: "+ date;
+    document.getElementById('time').innerHTML = time + " :: <br> "+ date;
 }, 1000);
